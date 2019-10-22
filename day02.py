e2bfffb6f847f1ed9199142e872e32cdbb640b34
Łukasz Szotowski <lukasz@szotowski.pl>
2019-10-22 20:26:47 +0200

    example worksheet

---
+++ day02.py
@@ -0,0 +1,32 @@
+# x = input("Podaj pierwszą liczbę: ")
+# y = input("Podaj drugą liczbę: ")
+# suma = int(x)+int(y)
+# print(suma)
+
+# exit()
+# this is a comment
+
+# tekst = "Ala ma kota"
+# ulamek = 1.34
+# ilosc_powtorzen = 4
+
+# print(ilosc_powtorzen*tekst)
+
+# tekst1 = "Tytul: \"Ala ma kota\""     # Tytul: "Ala ma kota"
+# tekst2 = """Tytul: "Ala ma kota" """  # Tytul: "Ala ma kota"
+# tekst3 = 'Tytul: "Ala ma kota"'       # Tytul: "Ala ma kota"
+# print(tekst1)
+# print(tekst2)
+# print(tekst3)
+#
+# tekst4 = r"test\test" # test\test
+# tekst5 = "test\\test" # test\test
+# print(tekst4)
+# print(tekst5)
+
+tekst = "Ala ma kota"
+print(tekst[0]) # A
+print(tekst[1]) # l
+print(tekst[2]) # a
+print(tekst[0:2]) # [0:2) Al
+print(tekst[len(tekst)-1])
