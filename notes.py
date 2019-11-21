e8e927291ecb9287a83c65b3dfe9986461161edd
Łukasz Szotowski <lukasz@szotowski.pl>
2019-11-21 20:23:56 +0100

    Day10.3

--- notes.py
+++ notes.py
@@ -1,10 +1,24 @@
 """
 openstrretmap - api do map, free
 
-
+https://github.com/python-visualization/folium
 """
 
 import folium
 
 m = folium.Map(location=[45.523,-122.6750])
+
+
+popup = "Rower mevo"
+tooltip = "Kliknij"
+
+rowery = [
+[45.523,-122.6750], #to moze byc dict{majacym lokalizacje geo i np naladowanie  baterii itp}
+[44.523,-122.6750]
+]
+
+for rower in rowery:
+    marker = folium.Marker(rower, popup=popup, tooltip=tooltip)
+    marker.add_to(m)
+
 m.save('index.html')
