e9f9b5bb15798ac6ae7f59e7714c60386ef5571f
Łukasz Szotowski <lukasz@szotowski.pl>
2019-11-26 19:34:24 +0100

    day11

--- day11.py
+++ day11.py
@@ -1,9 +1,15 @@
 import datetime
 
 class Pizza():
-
+    """
+    Cos o tej klasie.
+    """
     stawka_vat = 23 #pole klasy (poza konstruktorem, poprostu na poziomie klasy)
     __marza = 1.05 #prywatne pole (dwa podkreslniki z przodu)
+    """
+    # metoda prywatna też zaczyna się jej nazwa od dwóch podkreślników
+    """
+
 
     def __init__(self, skladnik_glowny):
         self.skladnik_glowny = skladnik_glowny
@@ -25,17 +31,79 @@ class Pizza():
     def podaj_date():
         return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-hawajska = Pizza("ananas")
-print(hawajska.skladnik_glowny)
+    #getter / setter / deleter
+    @property
+    def marza(self):
+        if self.__marza>=1.05:
+            return "Duzo"
+        else:
+            return "Malo"
+#
+# hawajska = Pizza("ananas")
+# print(hawajska.skladnik_glowny)
+#
+# hawajska2 = Pizza.Hawajska()
+# print(hawajska2.skladnik_glowny)
+#
+# print(Pizza.podaj_stawke_vat())
+# print(Pizza.stawka_vat)
+#
+# print(Pizza.podaj_date())
+#
+# print(hawajska.cena)
+# print(Pizza.__dict__)
+#
+# import pprint
+# pprint.pprint(Pizza.__dict__)
+#
+# print(Pizza._Pizza__marza)
+#
+# print(hawajska.marza)
+# # print(Pizza.marza())
 
-hawajska2 = Pizza.Hawajska()
-print(hawajska2.skladnik_glowny)
 
-print(Pizza.podaj_stawke_vat())
-print(Pizza.stawka_vat)
 
-print(Pizza.podaj_date())
+"""
+getery, setery, deletery
+"""
 
-print(hawajska.cena)
-print(Pizza.__dict__)
+class Student():
+
+    def __init__(self):
+        self.__imie = None
+        self.nazwisko = None
+        self.data_dodania = None
+        self.data_usuniecia = None
+        self.skasowany = False
+
+    @property #geter (ustawiamy na danych prywatnych)
+    def imie(self):
+        return self.__imie.capitalize()
+
+    @imie.setter
+    def imie(self, wartosc):
+        self.__imie = wartosc
+
+    @imie.deleter
+    def imie(self):
+        self.skasowany = True
+        self.data_usuniecia = self.pobierz_date()
+
+    @staticmethod
+    def pobierz_date():
+        return datetime.datetime.now().strftime('%Y-%m-%d')
+
+# student = Student()
+# student.imie = "lukasz"
+# student.nazwisko = "anonim"
+# student.data_dodania = student.pobierz_date()
+#
+# print(student.imie)
+# print(student.nazwisko)
+# print(student.data_dodania)
+# print(student.data_usuniecia)
+# print(student.skasowany)
+# del(student.imie)
+# print(student.data_usuniecia)
+# print(student.skasowany)
 
